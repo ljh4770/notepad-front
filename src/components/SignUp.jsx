554724ac8b8a3f6ac5b8577f617403fa8c78aa30
ljh4770 <ljh4770@naver.com>
2024-12-24 09:22:39 +0900

    env

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./styles/SignUp.css";
 
-const API_BASE = "http://127.0.0.1:8000/api";
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
 function SignUp() {
   const [formData, setFormData] = useState({
